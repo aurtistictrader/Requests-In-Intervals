c015556bb48bc3674cc2a603ea288bc60c2e42bb
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-17 03:10:04 -0500

    added server

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express')
 var app = express();
-var phantom = require('phantom');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -17,6 +16,7 @@ app.listen(app.get('port'), function() {
 
 
 function timed() {
+var phantom = require('phantom');
 	phantom.create(function (ph) {
 	  ph.createPage(function (page) {
 	    page.open("http://remind-me-yo.herokuapp.com/ghettoping", function (status) {
