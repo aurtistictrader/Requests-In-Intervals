3a1001078d4d5d8167f2ec099afd15b23b25c04f
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-17 04:26:18 -0500

    added phantom and buildpacks

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
-var phantom = require('phantom')
 var app = express();
+var assert=require('assert');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -11,12 +11,13 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
+	setInterval(timesd, 10000)
 
 })
 
 
-// setInterval(timed, 10000)
-function timed() {
+function timesd() {
+var phantom = require('phantom')
 	phantom.create(function (ph) {
 	  ph.createPage(function (page) {
 	    page.open("http://remind-me-yo.herokuapp.com/ghettoping", function (status) {
@@ -30,4 +31,38 @@ function timed() {
 	});
 }
 
+function timed() {
+var phantom=require('node-phantom');
+	phantom.create(function(error,ph){
+		assert.ifError(error);
+		ph.createPage(function(err,page){
+			assert.ifError(err);
+			page.open('http://localhost:5000',function(err,status){
+				assert.ifError(err);
+				assert.equal(status,'success');
+				ph.exit();
+				done();
+			});
+		});
+	});
+}
 
+
+// describe('Phantom Page',function(){
+// 	this.timeout(5000);
+// 	it('should be able to open',function(done){
+// 		phantom.create(function(error,ph){
+// 			assert.ifError(error);
+// 			ph.createPage(function(err,page){
+// 				assert.ifError(err);
+// 				page.open('http://localhost:'+server.address().port,function(err,status){
+// 					assert.ifError(err);
+// 					assert.equal(status,'success');
+// 					server.close();
+// 					ph.exit();
+// 					done();
+// 				});
+// 			});
+// 		});
+// 	});
+// });
