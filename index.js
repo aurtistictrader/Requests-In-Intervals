e83e4f4bcff6397770b6a88280b3db9fe873467b
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-17 02:45:31 -0500

    added phantom

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+var phantom = require('phantom');
+
+phantom.create(function (ph) {
+  ph.createPage(function (page) {
+    page.open("http://www.google.com", function (status) {
+      console.log("opened google? ", status);
+      page.evaluate(function () { return document.title; }, function (result) {
+        console.log('Page title is ' + result);
+        ph.exit();
+      });
+    });
+  });
+});
