f7f3da374808fc8d61ec5d88bb9adda4ea98ced0
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-17 02:58:30 -0500

    ghetto polling success

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-var express = require('express')
-var app = express();
+// var express = require('express')
+// var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+// app.set('port', (process.env.PORT || 5000))
+// app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+// app.get('/', function(request, response) {
+//   response.send('Hello World!')
+// })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
 
 var phantom = require('phantom');
 
-phantom.create(function (ph) {
-  ph.createPage(function (page) {
-    page.open("http://www.google.com", function (status) {
-      console.log("opened google? ", status);
-      page.evaluate(function () { return document.title; }, function (result) {
-        console.log('Page title is ' + result);
-        ph.exit();
-      });
-    });
-  });
-});
+function timed() {
+	phantom.create(function (ph) {
+	  ph.createPage(function (page) {
+	    page.open("http://remind-me-yo.herokuapp.com/ghettoping", function (status) {
+	      console.log("opened site? ", status);
+	      page.evaluate(function () { return document.title; }, function (result) {
+	        console.log('Page title is ' + result);
+	        ph.exit();
+	      });
+	    });
+	  });
+	});
+}
+setInterval(timed, 10000);
+
+
