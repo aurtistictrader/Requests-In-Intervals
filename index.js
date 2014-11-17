e46f411f0514a3660575b5a828d564e933499b90
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-17 01:52:17 -0500

    phantomjs buildpakc

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ app.get('/', function(request, response) {
   response.send('Evil phantom!')
 })
 
-var page = require('webpage').create(),
-    server = 'http://remind-me-yo.herokuapp.com/ghettoping';
-
-page.open(server, 'get', data, function (status) {
-    if (status !== 'success') {
-        console.log('Unable to get!');
-    } else {
-        console.log(page.content);
-    }
-    phantom.exit();
-});
-
 app.listen(app.get('port'), function() {
   console.log("Evil phantom is running at:" + app.get('port'))
 })
+
+// var page = require('webpage').create(),
+//     server = 'http://remind-me-yo.herokuapp.com/ghettoping';
+
+// page.open(server, 'get', data, function (status) {
+//     if (status !== 'success') {
+//         console.log('Unable to get!');
+//     } else {
+//         console.log(page.content);
+//     }
+//     phantom.exit();
+// });
