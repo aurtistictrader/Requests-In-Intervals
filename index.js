eb840c23d275d6130d0b21fa1ac6b933c351ba25
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-17 03:04:29 -0500

    added server

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-// var express = require('express')
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000))
-// app.use(express.static(__dirname + '/public'))
-
-// app.get('/', function(request, response) {
-//   response.send('Hello World!')
-// })
-
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'))
-// })
-
+var express = require('express')
+var app = express();
 var phantom = require('phantom');
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+  
+	setInterval(timed, 10000);
+})
+
 
 function timed() {
 	phantom.create(function (ph) {
@@ -27,6 +29,5 @@ function timed() {
 	  });
 	});
 }
-setInterval(timed, 10000);
 
 
