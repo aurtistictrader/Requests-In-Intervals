865a08183580150904f1453f93b2c98263b237da
Cheng Peng <cwpeng@uwaterloo.ca>
2014-11-17 03:16:56 -0500

    added server

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
   
-	// setInterval(timed, 10000);
 })
 
 
+setInterval(timed, 10000);
 function timed() {
 var phantom = require('phantom');
 	phantom.create(function (ph) {
